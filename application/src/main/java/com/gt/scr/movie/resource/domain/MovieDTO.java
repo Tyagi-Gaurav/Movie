17116f555b20ecd8fe254ee5e9965206d9d72394
Tyagi-Gaurav <aanconsultancyservicesltd@gmail.com>
2021-08-26 15:12:23 +0100

    43: Convert internal DTOs to record data types

--- application/src/main/java/com/gt/scr/movie/resource/domain/MovieDTO.java
+++ application/src/main/java/com/gt/scr/movie/resource/domain/MovieDTO.java
@@ -2,20 +2,13 @@ package com.gt.scr.movie.resource.domain;
 
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
-import org.immutables.value.Value;
 
 import java.math.BigDecimal;
 import java.util.UUID;
 
-@Value.Immutable
 @JsonSerialize
-@JsonDeserialize(builder = ImmutableMovieDTO.Builder.class)
-public interface MovieDTO {
-    UUID id();
-
-    String name();
-
-    int yearProduced();
-
-    BigDecimal rating();
-}
+@JsonDeserialize
+public record MovieDTO(UUID id,
+                       String name,
+                       int yearProduced,
+                        BigDecimal rating) {}
