38185508ad31073888018819bc941be0ad5c06d5
Tyagi-Gaurav <aanconsultancyservicesltd@gmail.com>
2021-11-04 16:51:29 +0000

    2: Refactored exception handling tests for better coverage

--- application/src/main/java/com/gt/scr/movie/exception/SystemExceptionHandler.java
+++ application/src/main/java/com/gt/scr/movie/exception/SystemExceptionHandler.java
@@ -3,12 +3,12 @@ package com.gt.scr.movie.exception;
 import com.gt.scr.movie.resource.domain.ErrorResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.support.WebExchangeBindException;
 import reactor.core.publisher.Mono;
 
 @ControllerAdvice
@@ -21,24 +21,6 @@ public class SystemExceptionHandler {
 
     public SystemExceptionHandler(ErrorResponseHelper errorResponseHelper) {
         this.errorResponseHelper = errorResponseHelper;
-    }
-
-    @ResponseStatus(code= HttpStatus.FORBIDDEN)
-    @ExceptionHandler(value = {UnauthorizedException.class})
-    public Mono<ErrorResponse> handleUnAuthorizedException(UnauthorizedException exception) {
-        if (LOG.isErrorEnabled()) {
-            LOG.error(exception.getMessage(), exception);
-        }
-        return errorResponseHelper.errorResponse(403, "Unauthorized");
-    }
-
-    @ResponseStatus(code= HttpStatus.BAD_REQUEST)
-    @ExceptionHandler(value = {WebExchangeBindException.class})
-    public Mono<ErrorResponse> handleGeneralValidationExceptions(WebExchangeBindException exception) {
-        if (LOG.isErrorEnabled()) {
-            LOG.error(exception.getMessage(), exception);
-        }
-        return errorResponseHelper.errorResponse(400, "Validation error occurred");
     }
 
     @ResponseStatus(code= HttpStatus.INTERNAL_SERVER_ERROR)
--- application/src/main/java/com/gt/scr/movie/exception/UnauthorizedExceptionHandler.java
+++ application/src/main/java/com/gt/scr/movie/exception/UnauthorizedExceptionHandler.java
@@ -4,28 +4,31 @@ import com.gt.scr.movie.resource.domain.ErrorResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import reactor.core.publisher.Mono;
 
 @ControllerAdvice
 @Component
 @Order(value = 1)
-public class IllegalArgumentExceptionHandler {
-    private static final Logger LOG = LoggerFactory.getLogger(IllegalArgumentExceptionHandler.class);
+public class UnauthorizedExceptionHandler {
+    private static final Logger LOG = LoggerFactory.getLogger(UnauthorizedExceptionHandler.class);
 
     private final ErrorResponseHelper errorResponseHelper;
 
-    public IllegalArgumentExceptionHandler(ErrorResponseHelper errorResponseHelper) {
+    public UnauthorizedExceptionHandler(ErrorResponseHelper errorResponseHelper) {
         this.errorResponseHelper = errorResponseHelper;
     }
 
-    @ExceptionHandler(value = {IllegalArgumentException.class})
-    public Mono<ErrorResponse> handle(IllegalArgumentException exception) {
+    @ResponseStatus(code= HttpStatus.FORBIDDEN)
+    @ExceptionHandler(value = {UnauthorizedException.class})
+    public Mono<ErrorResponse> handle(UnauthorizedException exception) {
         if (LOG.isErrorEnabled()) {
             LOG.error(exception.getMessage(), exception);
         }
-        return errorResponseHelper.errorResponse(400, exception.getMessage());
+        return errorResponseHelper.errorResponse(403, "Unauthorised");
     }
 }
