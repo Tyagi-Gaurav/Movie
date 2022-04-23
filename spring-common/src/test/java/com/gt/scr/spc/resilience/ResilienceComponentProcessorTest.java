d58f9e6e0909248dd8cf3149857fdc230794be20
Gaurav Tyagi <aanconsultancyservicesltd@gmail.com>
2022-04-23 22:24:24 +0100

    194: Fix code smells

--- spring-common/src/test/java/com/gt/scr/spc/resilience/ResilienceComponentProcessorTest.java
+++ spring-common/src/test/java/com/gt/scr/spc/resilience/ResilienceComponentProcessorTest.java
@@ -12,6 +12,7 @@ import org.junit.jupiter.api.Test;
 
 import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
+import java.time.Duration;
 import java.util.Map;
 import java.util.UUID;
 import java.util.concurrent.ExecutionException;
@@ -19,6 +20,7 @@ import java.util.concurrent.Executors;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+import static org.awaitility.Awaitility.await;
 
 class ResilienceComponentProcessorTest {
 
@@ -45,29 +47,30 @@ class ResilienceComponentProcessorTest {
         Proxy.getInvocationHandler(testProxy).invoke(testProxy, doSomethingWithResilience, new Object[]{input});
 
         assertThat(meterRegistry.timer("resilience4j.circuitbreaker.calls", "kind", "successful",
-                "name", "testBean.doSomethingWithResilience.circuitBreaker").count()).isEqualTo(1);
+                "name", "testBean.doSomethingWithResilience.circuitBreaker").count()).isOne();
     }
 
     @Test
     void shouldCircuitBreakWhenCallsFail() throws Throwable {
         String input = "testString-" + UUID.randomUUID();
-        int delay = 40;
-        Method doSomethingWithResilienceWithDelay = TestBean.class.getMethod("doSomethingWithResilienceWithDelay", String.class, Integer.class);
-        Proxy.getInvocationHandler(testProxy).invoke(testProxy, doSomethingWithResilienceWithDelay, new Object[]{input, delay});
+        int delay = 30;
+        Method doSomethingWithResilienceWithDelay = TestBean.class.getMethod("doSomethingWithResilienceWithDelayForCircuitBreaker", String.class, Integer.class);
+        Object[] args = {input, delay};
+        Proxy.getInvocationHandler(testProxy).invoke(testProxy, doSomethingWithResilienceWithDelay, args);
 
         assertThatExceptionOfType(ExecutionException.class).isThrownBy(() ->
-                Proxy.getInvocationHandler(testProxy).invoke(testProxy, doSomethingWithResilienceWithDelay, new Object[]{input, delay}));
+                Proxy.getInvocationHandler(testProxy).invoke(testProxy, doSomethingWithResilienceWithDelay, args));
 
-        String cbTag = "testBean.doSomethingWithResilienceWithDelay.circuitBreaker";
+        String cbTag = "testBean.doSomethingWithResilienceWithDelayForCircuitBreaker.circuitBreaker";
         assertThat(meterRegistry.get("resilience4j.circuitbreaker.slow.calls")
                 .tags("name", cbTag, "kind", "successful")
-                .gauge().value()).isEqualTo(1);
+                .gauge().value()).isOne();
 
         assertThat(meterRegistry.timer("resilience4j.circuitbreaker.calls", "kind", "successful",
-                "name", cbTag).count()).isEqualTo(1);
+                "name", cbTag).count()).isOne();
 
         assertThat(meterRegistry.get("resilience4j.circuitbreaker.state").tags("name", cbTag, "state", "open")
-                .gauge().value()).isEqualTo(1);
+                .gauge().value()).isOne();
     }
 
     @Test
@@ -77,13 +80,13 @@ class ResilienceComponentProcessorTest {
         Proxy.getInvocationHandler(testProxy).invoke(testProxy, doSomethingWithResilience, new Object[]{input});
 
         assertThat(meterRegistry.timer("resilience4j.circuitbreaker.calls", "kind", "successful",
-                "name", "testBean.doSomethingWithoutResilience.circuitBreaker").count()).isEqualTo(0);
+                "name", "testBean.doSomethingWithoutResilience.circuitBreaker").count()).isZero();
 
         assertThat(meterRegistry.timer("resilience4j.circuitbreaker.calls", "kind", "failed",
-                "name", "testBean.doSomethingWithoutResilience.circuitBreaker").count()).isEqualTo(0);
+                "name", "testBean.doSomethingWithoutResilience.circuitBreaker").count()).isZero();
 
         assertThat(meterRegistry.timer("resilience4j.circuitbreaker.calls", "kind", "ignored",
-                "name", "testBean.doSomethingWithoutResilience.circuitBreaker").count()).isEqualTo(0);
+                "name", "testBean.doSomethingWithoutResilience.circuitBreaker").count()).isZero();
     }
 
 
@@ -91,15 +94,15 @@ class ResilienceComponentProcessorTest {
     void shouldTimeoutWhenMethodTakesLongTime() throws Throwable {
         String input = "testString-" + UUID.randomUUID();
         int delay = 70;
-        Method doSomethingWithResilienceWithDelay = TestBean.class.getMethod("doSomethingWithResilienceWithDelay", String.class, Integer.class);
+        Method doSomethingWithResilienceWithDelay = TestBean.class.getMethod("doSomethingWithResilienceWithDelayForTimeout", String.class, Integer.class);
 
         assertThatExceptionOfType(ExecutionException.class).isThrownBy(() ->
                 Proxy.getInvocationHandler(testProxy).invoke(testProxy, doSomethingWithResilienceWithDelay, new Object[]{input, delay}));
 
-        String cbTag = "testBean.doSomethingWithResilienceWithDelay.timeLimiter";
+        String cbTag = "testBean.doSomethingWithResilienceWithDelayForTimeout.timeLimiter";
         assertThat(meterRegistry.get("resilience4j.timelimiter.calls")
                 .tags("name", cbTag, "kind", "timeout")
-                .counter().count()).isEqualTo(1);
+                .counter().count()).isOne();
 
     }
 
@@ -110,12 +113,15 @@ class ResilienceComponentProcessorTest {
         }
 
         @Resilience("testUpStream")
-        public Object doSomethingWithResilienceWithDelay(String arg, Integer delayInMs) {
-            try {
-                Thread.sleep(delayInMs);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
+        public Object doSomethingWithResilienceWithDelayForTimeout(String arg, Integer delayInMs) {
+            await().atLeast(Duration.ofMillis(delayInMs)).until(() -> true);
+            return arg;
+        }
+
+        @Resilience("testUpStream")
+        public Object doSomethingWithResilienceWithDelayForCircuitBreaker(String arg, Integer delayInMs) {
+            await().atLeast(Duration.ofMillis(delayInMs))
+                    .atMost(Duration.ofMillis(delayInMs)).until(() -> true);
             return arg;
         }
 
