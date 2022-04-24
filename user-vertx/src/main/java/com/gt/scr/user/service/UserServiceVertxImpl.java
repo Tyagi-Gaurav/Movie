161c39469ad104096236c46d7151ab70db7309e1
Gaurav Tyagi <aanconsultancyservicesltd@gmail.com>
2022-04-24 21:54:49 +0100

    194: Refactor bus send logic into separate class

--- user-vertx/src/main/java/com/gt/scr/user/service/UserServiceVertxImpl.java
+++ user-vertx/src/main/java/com/gt/scr/user/service/UserServiceVertxImpl.java
@@ -3,16 +3,15 @@ package com.gt.scr.user.service;
 import com.gt.scr.domain.User;
 import com.gt.scr.user.config.EventBusAddress;
 import com.gt.scr.user.exception.UserCreateException;
+import com.gt.scr.user.util.Bus;
 import io.vertx.core.Future;
 import io.vertx.core.Vertx;
-import io.vertx.core.eventbus.DeliveryOptions;
 import io.vertx.core.eventbus.ReplyException;
 import io.vertx.core.json.JsonObject;
 
 import java.util.UUID;
 
 public class UserServiceVertxImpl implements UserServiceV2 {
-
     private final Vertx vertx;
 
     public UserServiceVertxImpl(Vertx vertx) {
@@ -21,13 +20,7 @@ public class UserServiceVertxImpl implements UserServiceV2 {
 
     @Override
     public Future<JsonObject> add(User user) {
-        Long sendTimeout = vertx.getOrCreateContext()
-                .config()
-                .getLong("bus" + "." + EventBusAddress.REPO_ACCOUNT_CREATE.getAddress() + ".timeoutInMs",
-                        DeliveryOptions.DEFAULT_TIMEOUT);
-        return vertx.eventBus().request(EventBusAddress.REPO_ACCOUNT_CREATE.getAddress(),
-                        JsonObject.mapFrom(user),
-                        new DeliveryOptions().setSendTimeout(sendTimeout))
+        return Bus.request(vertx, EventBusAddress.REPO_ACCOUNT_CREATE.getAddress(), JsonObject.mapFrom(user))
                 .map(objectMessage -> {
                     Object body = objectMessage.body();
                     if (body instanceof ReplyException exp) {
@@ -40,45 +33,29 @@ public class UserServiceVertxImpl implements UserServiceV2 {
 
     @Override
     public Future<JsonObject> findByUsername(String userName) {
-        Long sendTimeout = vertx.getOrCreateContext()
-                .config()
-                .getLong("bus" + "." + EventBusAddress.REPO_USER_FETCH_BY_USER_NAME.getAddress() + ".timeoutInMs");
-        return vertx.eventBus().request(EventBusAddress.REPO_USER_FETCH_BY_USER_NAME.getAddress(),
-                        new JsonObject().put("userName", userName),
-                        new DeliveryOptions().setSendTimeout(sendTimeout))
+        return Bus.request(vertx, EventBusAddress.REPO_USER_FETCH_BY_USER_NAME.getAddress(),
+                        new JsonObject().put("userName", userName))
                 .map(msg -> (JsonObject) msg.body());
     }
 
     @Override
     public Future<JsonObject> findByUserId(UUID userId) {
-        Long sendTimeout = vertx.getOrCreateContext()
-                .config()
-                .getLong("bus" + "." + EventBusAddress.REPO_USER_FETCH_BY_USER_ID.getAddress() + ".timeoutInMs");
-        return vertx.eventBus().request(EventBusAddress.REPO_USER_FETCH_BY_USER_ID.getAddress(),
-                        new JsonObject().put("userId", userId.toString()),
-                        new DeliveryOptions().setSendTimeout(sendTimeout))
+        return Bus.request(vertx, EventBusAddress.REPO_USER_FETCH_BY_USER_ID.getAddress(),
+                        new JsonObject().put("userId", userId.toString()))
                 .map(msg -> (JsonObject) msg.body());
     }
 
     @Override
     public Future<JsonObject> delete(UUID userId) {
-        Long sendTimeout = vertx.getOrCreateContext()
-                .config()
-                .getLong("bus" + "." + EventBusAddress.REPO_USER_DELETE_BY_USER_ID.getAddress() + ".timeoutInMs");
-        return vertx.eventBus().request(EventBusAddress.REPO_USER_DELETE_BY_USER_ID.getAddress(),
-                new JsonObject().put("userId", userId.toString()),
-                        new DeliveryOptions().setSendTimeout(sendTimeout))
+        return Bus.request(vertx, EventBusAddress.REPO_USER_DELETE_BY_USER_ID.getAddress(),
+                new JsonObject().put("userId", userId.toString()))
                 .map(msg -> (JsonObject) msg.body());
     }
 
     @Override
     public Future<JsonObject> fetchAll() {
-        Long sendTimeout = vertx.getOrCreateContext()
-                .config()
-                .getLong("bus" + "." + EventBusAddress.REPO_USER_FETCH_ALL.getAddress() + ".timeoutInMs");
-        return vertx.eventBus().request(EventBusAddress.REPO_USER_FETCH_ALL.getAddress(),
-                        null,
-                        new DeliveryOptions().setSendTimeout(sendTimeout))
+        return Bus.request(vertx, EventBusAddress.REPO_USER_FETCH_ALL.getAddress(),
+                        null)
                 .map(msg -> (JsonObject) msg.body());
     }
 }
