fa0050219c76386fa6b5d5d9e017b5e1a849ca47
Gaurav Tyagi <aanconsultancyservicesltd@gmail.com>
2022-05-25 16:13:29 +0100

    340: Refactored to use direct byte buffer

--- java_scratch/src/main/java/com/gt/scr/imdb/ext/akas/AkasClient.java
+++ java_scratch/src/main/java/com/gt/scr/imdb/ext/akas/AkasClient.java
@@ -8,43 +8,39 @@ import reactor.core.publisher.Mono;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.RandomAccessFile;
 import java.net.URL;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class AkasClient implements AkasReader {
 
     public AkasClient(DataReader dataReader) {
         try {
-            final Set<String> objects = new HashSet<>();
             final Map<String, List<Integer>> keyMap = new HashMap<>();
             String fileName = "/title.akas.tsv";
             System.out.println("Before Start (In kb): " + Runtime.getRuntime().freeMemory() / 1000);
             final long startTime = System.currentTimeMillis();
-            InputStream resourceAsStream = TSVFileReader.class.getResourceAsStream(fileName);
             final URL resource = TSVFileReader.class.getResource(fileName);
             final RandomAccessFile randomAccessFile = new RandomAccessFile(new File(resource.getPath()), "r");
 
             final int BLOCK_SIZE = 2 * 1024 * 1024;
             int totalRow = 0;
-            final byte[] bytes = new byte[BLOCK_SIZE];
-            int off = 0;
             final AtomicInteger position = new AtomicInteger();
+            final FileChannel channel = randomAccessFile.getChannel();
+            final ByteBuffer byteBuffer = ByteBuffer.allocateDirect(BLOCK_SIZE);
             final ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
-            while (resourceAsStream.available() > 0) {
-                resourceAsStream.read(bytes, off, BLOCK_SIZE);
+            while (channel.read(byteBuffer) > 0) {
                 int i = 0;
-                while (i < bytes.length) {
-                    int end = getRow(i, bytes, byteArrayOutputStream, position);
-                    if (end + 1 < bytes.length && bytes[end] == 10) {
+                while (i < byteBuffer.limit()) {
+                    int end = getRow(i, byteBuffer, byteArrayOutputStream, position);
+                    if (end + 1 < byteBuffer.limit() && byteBuffer.get(end) == 10) {
                         final byte[] rowAsBytes = byteArrayOutputStream.toByteArray();
                         final String title = findTitle(rowAsBytes, position);
                         int finalI = i;
@@ -58,20 +54,14 @@ public class AkasClient implements AkasReader {
                                 return oldValue;
                             }
                         });
-                        //randomAccessFile.seek(i);
-                        //System.out.println("Row: " + randomAccessFile.readLine());
-                        //System.out.println(byteArrayOutputStream);
                         byteArrayOutputStream.reset();
                         totalRow++;
-                        //Thread.sleep(2000);
                     } else {
-//                        System.out.println("Reached end of stream now. Reading data again.: " + i + ", len: " + bytes.length + ", " +
-//                                "end: " + end);
-                        //Thread.sleep(2000);
                         break;
                     }
                     i = end + 1;
                 }
+                byteBuffer.clear();
             }
             final long endTime = System.currentTimeMillis();
             System.out.println("Total rows " + totalRow + ", time taken(In sec): " + (endTime - startTime)/1000);
@@ -97,12 +87,12 @@ public class AkasClient implements AkasReader {
         return new String(rowAsbytes, 0, position.get(), StandardCharsets.UTF_8);
     }
 
-    private int getRow(int start, byte[] bytes, ByteArrayOutputStream byteArrayOutputStream, AtomicInteger position) {
+    private int getRow(int start, ByteBuffer bytes, ByteArrayOutputStream byteArrayOutputStream, AtomicInteger position) {
         int i = start;
         boolean found = false;
-        while (i < bytes.length && bytes[i] != 10) {
-            byteArrayOutputStream.write(bytes[i]);
-            if (!found && bytes[i] == 9) {
+        while (i < bytes.limit() && bytes.get(i) != 10) {
+            byteArrayOutputStream.write(bytes.get(i));
+            if (!found && bytes.get(i) == 9) {
                 position.set(i - start);
                 found = true;
             }
@@ -114,7 +104,7 @@ public class AkasClient implements AkasReader {
 
     @Override
     public Mono<Aka> getTitleById(String titleId) {
-        return Mono.empty(); //justOrEmpty(akasMap.get(titleId));
+        return Mono.empty();
     }
 
     @Override
