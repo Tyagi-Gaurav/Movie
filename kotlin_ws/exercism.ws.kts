fde32a5c87deecfa9cbf75ea9359727439eb169c
Gaurav Tyagi <aanconsultancyservicesltd@gmail.com>
2022-06-06 21:36:39 +0100

    340: Exercism worksheet

--- kotlin_ws/exercism.ws.kts
+++ kotlin_ws/exercism.ws.kts
@@ -1,4 +1,7 @@
 import java.time.*
+import java.util.Comparator
+import java.util.stream.IntStream as IntStream1
+import java.util.stream.Collectors
 
 object Hamming {
 
@@ -125,10 +128,9 @@ enum class Signal {
     WINK, DOUBLE_BLINK, CLOSE_YOUR_EYES, JUMP
 }
 
-
 object HandshakeCalculator {
     fun calculateHandshake(number: Int): List<Signal> {
-        return with (arrayListOf<Signal>()) {
+        return with(arrayListOf<Signal>()) {
             if (number and 1 == 1) add(Signal.WINK)
             if (number and 2 == 2) add(Signal.DOUBLE_BLINK)
             if (number and 4 == 4) add(Signal.CLOSE_YOUR_EYES)
@@ -139,4 +141,75 @@ object HandshakeCalculator {
     }
 }
 
-println (HandshakeCalculator.calculateHandshake(24))
+println(HandshakeCalculator.calculateHandshake(24))
+
+//out is called variance annotation. Triangle is producer of T's
+//not consumer of T's. Below it provides declaration site variance.
+//T is a covariant type parameter. C is producer of T's and not
+//consumer of T's.
+class Triangle<out T : Number>(val a: T, val b: T, val c: T) {
+
+    init {
+        fun verifiesInequality(a: T, b: T, c: T) = a.toDouble() > b.toDouble() + c.toDouble()
+
+        if (a == 0 || b == 0 || c == 0
+            || verifiesInequality(a, b, c)
+            || verifiesInequality(b, a, c)
+            || verifiesInequality(c, a, b)
+        ) {
+            throw IllegalArgumentException()
+        }
+    }
+
+    val isEquilateral: Boolean = a == b && b == c
+    val isIsosceles: Boolean = isEquilateral || (a == b && b != c) || (a == c && c != b) || (b == c && c != a)
+    val isScalene: Boolean = a != b && b != c && c != a
+}
+//val t = Triangle(0, 0, 0)
+//val t1 = Triangle(7, 1, 1)
+
+//Saddle points
+data class MatrixCoordinate(val row: Int, val col: Int)
+
+class MatrixInt(val rawMatrix: List<List<Int>>) {
+    val rows = rawMatrix.size
+    val cols = if (rawMatrix.isEmpty())  0 else rawMatrix[0].size
+
+    val saddlePoints = calculate()
+
+    fun calculate() : Set<MatrixCoordinate> {
+        val result = mutableSetOf<MatrixCoordinate>()
+
+        //Calculate max for each row
+        val maxRowSet = rawMatrix.map { it -> it.maxOrNull() }
+
+        //println(maxRowSet)
+
+        val minColSet = (0..cols - 1)
+            .map { it -> rawMatrix.map { l -> l[it] }}
+            .map { it -> it.minOrNull()}
+
+        //println(minColSet)
+
+        //Return intersection of two sets
+        for ((rowIndex, row) in rawMatrix.withIndex()) {
+            for ((colIndex, _) in row.withIndex()) {
+                val cell = rawMatrix[rowIndex][colIndex]
+                if (cell == maxRowSet[rowIndex] && cell == minColSet[colIndex]) {
+                    result.add(MatrixCoordinate(rowIndex +1, colIndex + 1))
+                }
+            }
+        }
+        return result
+    }
+}
+
+val m = MatrixInt(
+    listOf(
+        listOf(9, 8, 7),
+        listOf(5, 3, 2),
+        listOf(6, 6, 7)
+    )
+)
+
+m.saddlePoints()
