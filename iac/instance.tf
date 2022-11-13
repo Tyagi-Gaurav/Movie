3e092fee1d72cb4c4d5fecb1011e3e9dbdade165
Gaurav Tyagi <aanconsultancyservicesltd@gmail.com>
2022-11-13 22:57:14 +0000

    344: Writing private IPs to tmp file

--- iac/instance.tf
+++ iac/instance.tf
@@ -7,6 +7,9 @@ resource "aws_instance" "example" {
   ami           = lookup(var.AMIS, var.AWS_REGION)
   instance_type = "t2.micro"
   key_name      = aws_key_pair.myKey.key_name
+  provisioner "local-exec" {
+    command = "echo ${aws_instance.example.private_ip} >> /tmp/private_ips.txt"
+  }
 }
 
 resource "null_resource" "provision" {
@@ -21,7 +24,7 @@ resource "null_resource" "provision" {
     ]
   }
   connection {
-    host = aws_instance.example.public_ip
+    host        = aws_instance.example.public_ip
     user        = var.INSTANCE_USERNAME
     private_key = file("${var.PATH_TO_PRIVATE_KEY}") #Create keys using keygen - ssh-keygen -f mykey
   }
