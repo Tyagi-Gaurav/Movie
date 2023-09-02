ed0fa39db868ceb45215e4c9dac74fe36da6ae3f
Gaurav Tyagi <aanconsultancyservicesltd@gmail.com>
2023-09-02 15:48:32 +0100

    Node specific folders

---
+++ ui/udemy_course/2.3 NPM/index.js
@@ -0,0 +1,10 @@
+//npm init
+//npm i sillyname
+import generateName from "sillyname";
+import superheroes from "superheroes";
+
+var sillyname = generateName();
+
+console.log(`My name is ${sillyname}`);
+
+console.log(`I am ${superheroes.random()}`)
