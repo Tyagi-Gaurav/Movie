6e1dc1f1ba62055bbd01a5aea912e6e57971a422
Gaurav Tyagi <aanconsultancyservicesltd@gmail.com>
2023-09-08 17:35:14 +0100

    Removed UI course

--- ui/udemy_course/DOM Challenge Starting Files/index.js
+++
@@ -1,9 +0,0 @@
-document.querySelector("#list li").firstElementChild.style.color = "red";
-
-document.querySelector("button").style.backgroundColor = "yellow";
-
-//can also be used to apply HTML
-document.querySelector("h1").innerHTML = "";
-
-//Text content of H1
-document.querySelector("h1").textContent = "";
